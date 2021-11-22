fe89616e0d2729674c7ffaadbd73eaa2081b83d5
Monica Eugene De Ocampo <86217952+eugene-deocampo@users.noreply.github.com>
2021-11-22 12:12:11 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("sql6.freemysqlhosting.net", "sql6452751", "Qq9N1k7q7i", "sql6452751");
+$mysqli = new mysqli("remotemysql.com", "BuYLnm8fmi", "byEsTIfXUu", "BuYLnm8fmi");
 
 if(mysqli -> connect_errno) {
 	echo "Failed to Connect to MySQL: " .$mysqli -> connect_error;
