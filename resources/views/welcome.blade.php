32bb2d672133e900b580cd2443d50f0075861a7c
Monica Eugene De Ocampo <86217952+eugene-deocampo@users.noreply.github.com>
2021-11-22 12:38:15 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,7 +76,7 @@
                                      </button>
 
                                      <div class="text-center">
-                                        <a class="small" href="{{ route('forgotpassword') }}">Forgot Password</a>
+                                        <a class="small" href="">Forgot Password</a>
                                         </div>
 
                                 </form>
