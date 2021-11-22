1d41626ae92686c3c5a682f702b8a91a6875e135
Monica Eugene De Ocampo <86217952+eugene-deocampo@users.noreply.github.com>
2021-11-22 12:19:52 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 $mysqli = new mysqli("remotemysql.com", "BuYLnm8fmi", "byEsTIfXUu", "BuYLnm8fmi");
 
 if(mysqli -> connect_errno) {
-	echo "Failed to Connect to MySQL: " .$mysqli -> connect_error;
+	echo "Failed to Connect to MySQL: " . $mysqli -> connect_error;
 	exit();
 } else {
 	echo "Wahoo! Your database is connected";
